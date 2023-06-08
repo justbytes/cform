ae6518c336160462ac2e16c946b31b79d69661da
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-07 21:02:07 -0700

    heroku

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const usersRouter = require('./users-router');
 const postRouter = require('./post-router');
 
 // Express routes
-router.use('/users', usersRouter);
-router.use('/post', postRouter);
+router.use('https://cform.herokuapp.com/users', usersRouter);
+router.use('https://cform.herokuapp.com/post', postRouter);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const homeRouter = require('./home-router');
 const apiRouter = require('./api');
 
-router.use('/', homeRouter);
-router.use('/api', apiRouter);
+router.use('https://cform.herokuapp.com/', homeRouter);
+router.use('https://cform.herokuapp.com/api', apiRouter);
 
 module.exports = router;
